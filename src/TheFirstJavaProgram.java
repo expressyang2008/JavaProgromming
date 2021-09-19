ef8db14dcac1351e1f6406765b60e90ed958a57d
shawnyang <expressyang@gmail.com>
2021-09-19 17:11:35 +0800

    This is a test

--- src/TheFirstJavaProgram.java
+++ src/TheFirstJavaProgram.java
@@ -5,7 +5,19 @@
  * @create: 2021-09-19 01:51
  */
 public class TheFirstJavaProgram {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    /**
+    * @Description:
+    * @param: x,y
+    * @return:
+    * @Author: Shawn Yang
+    * @DateTime: 2021/9/19 5:04 下午
+    */
+    private int add(int x, int y) {
+        return x + y;
     }
 }
