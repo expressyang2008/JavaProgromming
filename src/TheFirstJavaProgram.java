50ed7f52d93c3fa08eb6f7803338bc6ef3416f41
shawnyang <expressyang@gmail.com>
2021-09-19 14:10:02 +0800

    This is a test

--- src/TheFirstJavaProgram.java
+++ src/TheFirstJavaProgram.java
@@ -6,6 +6,6 @@
  */
 public class TheFirstJavaProgram {
     public static void main(String[] args){
-        System.out.println("This is a test!");
+        System.out.println("Hello World!");
     }
 }
